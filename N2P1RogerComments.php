c7de51cc9a84821e824990fa09ba1a0a64b721b1
daw2-baron21 <71894827+daw2-baron21@users.noreply.github.com>
2020-10-21 20:27:19 +0200

    Create N2P1RogerComments.php

---
+++ N2P1RogerComments.php
@@ -0,0 +1,39 @@
+<html>
+
+
+
+    <h1>Lista 10 cosas importantes aprendidas:</h1>
+    
+    <p>Utilizar la etiqueta date para asignar fechas determinadas<br></p>
+
+    <p>Crear en definitiva un documento php y que funcione<br></p>
+    
+    <p>Linkar los documentos php para enviar información y acceder a otras páginas<br></p>
+
+    <p>Crear cookies y establecer un tiempo de asignación para la misma<br></p>
+
+    <p>Recibir las variables session<br></p>
+
+    <p>Establecer zona horaria<br></p>
+
+    <p>Configurar valores vaciós con el comando null<br></p>
+
+    <p>Recibir datos en otro documento con post<br></p>
+
+    <p>Crear datos especificos y poder utilizarlos en otro documento simultaneamente.<br></p>
+
+    <p>Utilizar la funcion urlencode para enviar datos con espaciado mediante la url.<br></p>
+
+    <h1>Documento y explicación del professor:</h1>
+
+    <p>Documento=7; Explicación del professor=7.<br></p>
+
+    <h1>Autoevaluacion:</h1>
+
+    <p>7.<br></p>
+
+    <h1>Mejoras:</h1>
+
+    <p>Nada destacable<br></p>    
+
+</html>
